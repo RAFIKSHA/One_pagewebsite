32fd7304a39fce0b40532a927296373930587dbc
RAFIKSHA <rafikshah3262gmail.com>
2024-09-07 18:23:07 +0530

    added templates and changes in setting

--- simple/simple/urls.py
+++ simple/simple/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from simple import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("about-us/",views.aboutus),
+    path("",views.home,name="home page"),
 ]
